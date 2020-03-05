595e8968bcbc2301e153fb0ac1784f66312b9641
Jordan Addison <jordanrileyaddison@gmail.com>
2020-03-04 22:49:32 -0600

    fixes.

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,9 +1,17 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import preloadAll from "jest-next-dynamic";
+import { render, screen, act } from "@testing-library/react";
 import IndexPage from "pages";
+
+// this will make sure next/dynamic works properly with our test.
+beforeAll(async () => {
+  await preloadAll();
+});
 
 test("The app renders", () => {
   expect.assertions(1);
-  render(<IndexPage />);
-  expect(screen.queryByText("My Nextjs Boilerplate.")).toBeDefined();
+  act(() => {
+    render(<IndexPage />);
+    expect(screen.queryByText("My Nextjs Boilerplate.")).toBeDefined();
+  });
 });
